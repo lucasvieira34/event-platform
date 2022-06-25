5c43c0b900576e7ff92444892b32b99df69c66d3
Lucas Teixeira Vieira <lucas.teixeira34@gmail.com>
2022-06-24 22:36:44 -0300

    - feat: implementando mutation de subscriber.

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4oxup421eqw01xke9c19r7t/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESSKEY_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
